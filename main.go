51c7d453afdbc643d73965ad8f11d3c07462a310
Boris Yakimov <boris.yakimov@itgix.com>
2021-11-14 11:42:31 +0200

    test implement vpc in network package

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
+	//"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+
+	"automation-as-a-service/network"
 )
+
+func init() {
+		vpcCidrRange := "10.0.0.0/16"
+}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "boris-test", nil)
-		if err != nil {
-			return err
+
+		createVpcErr := network.CreateVPC(ctx, vpcCidrRange)
+		if createVpcErr != nil {
+			return createVpcErr
 		}
 
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		// Create an AWS resource (S3 Bucket)
+		//bucket, err := s3.NewBucket(ctx, "boris-test", nil)
+		//if err != nil {
+			//return err
+		//}
+
+		//// Export the name of the bucket
+		//ctx.Export("bucketName", bucket.ID())
 		return nil
 	})
 }
--- network/vpc.go
+++ network/vpc.go
@@ -1,7 +1,16 @@
 package network
 
-import "fmt"
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
 
-func main() {
-	fmt.Println("vim-go")
+func CreateVPC(ctx *pulumi.Context, vpcCidrRange string) error {
+	_, createVpcErr := ec2.NewVpc(ctx, "main", &ec2.VpcArgs{
+		CidrBlock: pulumi.String(vpcCidrRange),
+	})
+	if createVpcErr != nil {
+		return createVpcErr
+	}
+	return nil
 }
