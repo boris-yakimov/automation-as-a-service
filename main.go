4daef2064f61213d6a59de2649c3ab69a248ef90
Boris Yakimov <yakimovb@gmail.com>
2021-11-19 18:14:45 +0200

    update format

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"fmt"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 
 	"automation-as-a-service/network"
 )
--- network/internet_gateway.go
+++ network/internet_gateway.go
@@ -5,14 +5,13 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-
 func CreateInternetGateway(ctx *pulumi.Context, projectName string, vpcId string) (igwConfigObject *ec2.InternetGateway, createIgwErr error) {
 	igwName := projectName + "-vpc"
 
 	igwConfig, createIgwErr := ec2.NewInternetGateway(ctx, igwName, &ec2.InternetGatewayArgs{
 		VpcId: pulumi.String(vpcId),
 		Tags: pulumi.StringMap{
-			"Name": pulumi.String(igwName),
+			"Name":      pulumi.String(igwName),
 			"ManagedBy": pulumi.String("Pulumi"),
 		},
 	})
