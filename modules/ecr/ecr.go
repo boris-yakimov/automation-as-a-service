a86b4157eeead6ca97c34c3e8c053fa36bad426a
Boris Yakimov <boris.yakimov@itgix.com>
2023-11-19 20:48:06 +0200

    fix ecr repo and lifecyecl policy creation

--- modules/ecr/ecr.go
+++ modules/ecr/ecr.go
@@ -1,44 +1,41 @@
 package ecr
 
 import (
-	"fmt"
-
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ecr"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func CreateECR(ctx *pulumi.Context, projectName string, listOfEcrRepos map[string]string) (ecrResourceObject *ecr.Repository, createEcrError error) {
+func CreateECR(ctx *pulumi.Context, projectName string, ecrRepoName string) (ecrResourceObject *ecr.Repository, createEcrError error) {
 	var ecrResource *ecr.Repository
 
 	// TODO: add optional flag if ImageScanning should be enabled - low prio
-
-	for _, ecrRepoName := range listOfEcrRepos {
-		// TODO: ecrResource should be in a map of all repos that is returned at the end
-		ecrResource, createEcrError = ecr.NewRepository(ctx, ecrRepoName, &ecr.RepositoryArgs{
-			// TODO: change to name from map
-			//Name: pulumi.String(ecrRepo),
-			ImageScanningConfiguration: &ecr.RepositoryImageScanningConfigurationArgs{
-				ScanOnPush: pulumi.Bool(true),
-			},
-			ImageTagMutability: pulumi.String("MUTABLE"),
-			Tags: pulumi.StringMap{
-				"Name":      pulumi.String("testEcrRepo"),
-				"ManagedBy": pulumi.String("pulumi"),
-				"Project":   pulumi.String(projectName),
-			},
-		})
-		if createEcrError != nil {
-			return nil, createEcrError
-		}
+	ecrResource, createEcrError = ecr.NewRepository(ctx, ecrRepoName, &ecr.RepositoryArgs{
+		ImageScanningConfiguration: &ecr.RepositoryImageScanningConfigurationArgs{
+			ScanOnPush: pulumi.Bool(true),
+		},
+		ImageTagMutability: pulumi.String("MUTABLE"),
+		Tags: pulumi.StringMap{
+			"Name":      pulumi.String(ecrRepoName),
+			"ManagedBy": pulumi.String("pulumi"),
+			"Project":   pulumi.String(projectName),
+		},
+	})
+	if createEcrError != nil {
+		return nil, createEcrError
 	}
 
 	// TODO: this has to be a map of objects, even if it contains a single one
 	return ecrResource, nil
 }
 
-func ConfigureEcrLifecyclePolicy(ctx *pulumi.Context, ecrLifecyclePolicyName string, imageRetentionPeriod string) (configureEcrLifecyclePolicyErr error) {
-	lifeCyclePolicyJson := fmt.Sprintf(`{
-   "rules": [
+// TODO: for some reason those are still trying to be created before the actual ECR repos are, although they have both a dependency and a parent set
+func ConfigureEcrLifecyclePolicy(ctx *pulumi.Context, ecrRepoName string, ecrLifecyclePolicyName string, imageRetentionPeriod string, ecrRepoResource *ecr.Repository) (configureEcrLifecyclePolicyErr error) {
+	//lifeCyclePolicyJson :=
+
+	_, configureEcrLifecyclePolicyErr = ecr.NewLifecyclePolicy(ctx, ecrLifecyclePolicyName, &ecr.LifecyclePolicyArgs{
+		Repository: pulumi.String(ecrRepoName),
+		Policy: pulumi.Any(`{
+		   "rules": [
 				{
 					"rulePriority": 1,
 					"description": "Expire images older than 14 days",
@@ -59,31 +56,18 @@ func ConfigureEcrLifecyclePolicy(ctx *pulumi.Context, ecrLifecyclePolicyName str
 						"tagStatus": "any",
 						"countType": "sinceImagePushed",
 						"countUnit": "days",
-						"countNumber": %s
-					},
-					"action": {
-						"type": "expire"
-					}
-				},
-				{
-					"rulePriority": 3,
-					"description": "Keep at most 100 images, expire all others",
-					"selection": {
-						"tagStatus": "any",
-						"countType": "imageCountMoreThan",
-						"countNumber": 100
+						"countNumber": 90 
 					},
 					"action": {
 						"type": "expire"
 					}
 				}
 			]
-		}`, imageRetentionPeriod)
-
-	_, configureEcrLifecyclePolicyErr = ecr.NewLifecyclePolicy(ctx, ecrLifecyclePolicyName, &ecr.LifecyclePolicyArgs{
-		Repository: pulumi.String(ecrLifecyclePolicyName),
-		Policy:     pulumi.Any(lifeCyclePolicyJson),
-	})
+		}`),
+	},
+		pulumi.Parent(ecrRepoResource),
+		pulumi.DependsOn([]pulumi.Resource{ecrRepoResource}),
+	)
 	if configureEcrLifecyclePolicyErr != nil {
 		return configureEcrLifecyclePolicyErr
 	}
--- provisioning/ecr.go
+++ provisioning/ecr.go
@@ -2,24 +2,31 @@ package provisioning
 
 import (
 	"automation-as-a-service/modules/ecr"
+	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func Ecr(ctx *pulumi.Context, projectName string, listOfEcrRepos map[string]string) (createListOfEcrReposErr error) {
-	_, createListOfEcrReposErr = ecr.CreateECR(ctx, projectName, listOfEcrRepos)
-	if createListOfEcrReposErr != nil {
-		return createListOfEcrReposErr
-	}
+	// TODO: ecrResource should be in a map of all repos that is returned at the end
+	// Create ECR repos
+	for _, ecrRepoName := range listOfEcrRepos {
+		ecrRepoResource, createListOfEcrReposErr := ecr.CreateECR(ctx, projectName, ecrRepoName)
+		if createListOfEcrReposErr != nil {
+			return createListOfEcrReposErr
+		}
 
-	// TODO: figure out where we can get those from - maybe an env var or lists with config options (or config file with config options) - for now it is hardcoded as true
-	var enableEcrLifecyclePolicy bool = true
-	var imageRetentionPeriodInDays = "90"
-	var ecrLifecyclePolicyName = "image-cleanup-policy"
-	if enableEcrLifecyclePolicy {
-		configureEcrLifecycleErr := ecr.ConfigureEcrLifecyclePolicy(ctx, ecrLifecyclePolicyName, imageRetentionPeriodInDays)
-		if configureEcrLifecycleErr != nil {
-			return configureEcrLifecycleErr
+		var enableEcrLifecyclePolicy bool = true
+		var imageRetentionPeriodInDays = "90"
+		var ecrLifecyclePolicyName = fmt.Sprintf("%s-lifecycle-policy", ecrRepoName)
+		if enableEcrLifecyclePolicy {
+			// Attach lifecycle policy to each ECR repo
+			// TODO: figure out where we can get those from - maybe an env var or lists with config options (or config file with config options) - for now it is hardcoded as true
+
+			configureEcrLifecycleErr := ecr.ConfigureEcrLifecyclePolicy(ctx, ecrRepoName, ecrLifecyclePolicyName, imageRetentionPeriodInDays, ecrRepoResource)
+			if configureEcrLifecycleErr != nil {
+				return configureEcrLifecycleErr
+			}
 		}
 	}
 
