d963f6319283c09db1894c9fb15553246bddc4f9
Boris Yakimov <yakimovb@gmail.com>
2023-12-01 22:37:12 +0200

    #8 dynamic route table id; validation route table list

--- modules/network/vpc_endpoints.go
+++ modules/network/vpc_endpoints.go
@@ -2,28 +2,36 @@ package network
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func CreateS3VpcEndpoint(ctx *pulumi.Context, projectName string, mainRegion string, vpcResource *ec2.Vpc, listOfPrivateRouteTables []*ec2.RouteTable) (vpcEndpointResource *ec2.VpcEndpoint, createS3VpcEndpointErr error) {
+
+	if len(listOfPrivateRouteTables) < 2 {
+		currentLen := strconv.Itoa(len(listOfPrivateRouteTables))
+		return nil, fmt.Errorf("listOfPrivateRouteTables should contain exactly 3 route tables, it currently contains %s", currentLen)
+	}
+
 	vpcEndpointName := fmt.Sprintf("%s-s3-vpc-gateway-endpoint", projectName)
 	s3ServiceName := fmt.Sprintf("com.amazonaws.%s.s3", mainRegion)
+
 	_, createS3VpcEndpointErr = ec2.NewVpcEndpoint(ctx, vpcEndpointName, &ec2.VpcEndpointArgs{
 		VpcId:       pulumi.StringInput(vpcResource.ID()),
 		ServiceName: pulumi.String(s3ServiceName),
-		// TODO: switch this to non hardcoded but dynamic values for route table id
 		RouteTableIds: pulumi.StringArray{
-			pulumi.String("rtb-0b63b6fa519b6c77e"),
-			pulumi.String("rtb-038a88c6f1f0d4258"),
-			pulumi.String("rtb-089010a8ca4c5b45e"),
+			pulumi.StringInput(listOfPrivateRouteTables[0].ID()),
+			pulumi.StringInput(listOfPrivateRouteTables[1].ID()),
+			pulumi.StringInput(listOfPrivateRouteTables[2].ID()),
 		},
 		Tags: pulumi.StringMap{
 			"Name":      pulumi.String(vpcEndpointName),
 			"ManagedBy": pulumi.String("pulumi"),
 		},
 	})
+
 	if createS3VpcEndpointErr != nil {
 		return nil, createS3VpcEndpointErr
 	}
@@ -32,8 +40,15 @@ func CreateS3VpcEndpoint(ctx *pulumi.Context, projectName string, mainRegion str
 }
 
 func CreateDynamoDBVpcEndpoint(ctx *pulumi.Context, projectName string, mainRegion string, vpcResource *ec2.Vpc, listOfPrivateRouteTables []*ec2.RouteTable) (vpcEndpointResource *ec2.VpcEndpoint, createDynamoVpcEndpointErr error) {
+
+	if len(listOfPrivateRouteTables) < 2 {
+		currentLen := strconv.Itoa(len(listOfPrivateRouteTables))
+		return nil, fmt.Errorf("listOfPrivateRouteTables should contain exactly 3 route tables, it currently contains %s", currentLen)
+	}
+
 	vpcEndpointName := fmt.Sprintf("%s-dynamo-vpc-gateway-endpoint", projectName)
 	dynamodbServiceName := fmt.Sprintf("com.amazonaws.%s.dynamodb", mainRegion)
+
 	_, createDynamoVpcEndpointErr = ec2.NewVpcEndpoint(ctx, vpcEndpointName, &ec2.VpcEndpointArgs{
 		VpcId:       pulumi.StringInput(vpcResource.ID()),
 		ServiceName: pulumi.String(dynamodbServiceName),
@@ -47,6 +62,7 @@ func CreateDynamoDBVpcEndpoint(ctx *pulumi.Context, projectName string, mainRegi
 			"ManagedBy": pulumi.String("pulumi"),
 		},
 	})
+
 	if createDynamoVpcEndpointErr != nil {
 		return nil, createDynamoVpcEndpointErr
 	}
