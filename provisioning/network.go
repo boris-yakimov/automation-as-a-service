095eec43fa8fcf0641dcea162b7bf8611b10ef90
Boris Yakimov <yakimovb@gmail.com>
2023-12-01 23:42:37 +0200

    #1 i don't know what going on anymore

--- provisioning/network.go
+++ provisioning/network.go
@@ -27,10 +27,10 @@ func Network(ctx *pulumi.Context, projectName string, mainRegion string, vpcCidr
 	}
 
 	// TODO: check if I can automate handling of request to increase max number of IPs in account - creating EC2 EIP: AddressLimitExceeded: The maximum number of addresses has been reached.
-
 	// Public Subnets - NAT gateway and Route tables
 	var listOfNatGateways []*ec2.NatGateway
 	privateSubnets := make(map[string]*ec2.Subnet)
+	natGateways := make(map[string]*ec2.NatGateway)
 
 	for subnetName, cidrRange := range subnetList {
 		var subnetType string
@@ -45,7 +45,7 @@ func Network(ctx *pulumi.Context, projectName string, mainRegion string, vpcCidr
 		var currentSubnet *ec2.Subnet
 
 		currentSubnet, createSubnetErr = network.CreateSubnet(ctx, projectName, subnetType, subnetName, cidrRange, vpcResource)
-		fmt.Printf("Created subnet: %s (%s) with CIDR: %s\n", subnetName, subnetType, cidrRange)
+		//fmt.Printf("Created subnet: %s (%s) with CIDR: %s\n", subnetName, subnetType, cidrRange)
 		if createSubnetErr != nil {
 			return createSubnetErr
 		}
@@ -54,11 +54,13 @@ func Network(ctx *pulumi.Context, projectName string, mainRegion string, vpcCidr
 
 		if subnetType == "public" {
 			currentNatGateway, createNatGwErr := network.CreateNatGateway(ctx, projectName, indexNum, currentSubnet, vpcResource)
-			fmt.Printf("Created NAT Gateway for subnet %s\n", subnetName)
+			//fmt.Printf("Created NAT Gateway for subnet %s\n", subnetName)
 			if createNatGwErr != nil {
 				return createNatGwErr
 			}
+			// TODO: convert this to a map of cidr to nat to force an order !!!
 			listOfNatGateways = append(listOfNatGateways, currentNatGateway)
+			natGateways[cidrRange] = currentNatGateway
 
 			routeTablePublic, createIgwRouteTableErr := network.CreatePublicRouteTable(ctx, projectName, indexNum, vpcResource, "public", "0.0.0.0/0", inetGwResource)
 			if createIgwRouteTableErr != nil {
@@ -76,40 +78,73 @@ func Network(ctx *pulumi.Context, projectName string, mainRegion string, vpcCidr
 		}
 	}
 
-	// Sort list of CIDR ranges and their associated to NAT gateways to prevent random assignment of route table to nat gateway that constantly detects route table changes
+	// Sort list of CIDR ranges and their associated to NAT gateways to prevent random assignment of route table to NAT gateway that constantly detects route table changes
 	sortedCidrRanges := make([]string, 0, len(privateSubnets))
 	for k := range privateSubnets {
 		sortedCidrRanges = append(sortedCidrRanges, k)
 	}
-	// TODO: make sure to understand how stable slice works here and what is the different between the regular sort
-	//sort.Strings(sortedCidrRanges)
+	// TODO: make sure to understand how stable slice works here and what is the difference between the regular sort
 	sort.SliceStable(sortedCidrRanges, func(i, j int) bool {
 		return sortedCidrRanges[i] < sortedCidrRanges[j]
 	})
 
-	natGateways := make(map[string]*ec2.NatGateway)
-	for i, natResource := range listOfNatGateways {
-		cidr := sortedCidrRanges[i]
-		natGateways[cidr] = natResource
-	}
-	fmt.Printf("NAT Gateway Assignments: %v\n", natGateways)
+	//natGateways := make(map[string]*ec2.NatGateway)
+	//for i, natResource := range listOfNatGateways {
+	//cidr := sortedCidrRanges[i]
+	//natGateways[cidr] = natResource
+	//}
+	// debug
+	//fmt.Printf("NAT Gateway Assignments: %v\n", natGateways)
+	// TODO: fix name
+	sortedRouteTables := make(map[string]*ec2.RouteTable)
 
 	var listOfPrivateRouteTables []*ec2.RouteTable
+
 	// Private Subnets - Route Tables and VPC Endpoints
-	for i, cidrRange := range sortedCidrRanges {
-		indexNum := strconv.Itoa(i + 1)
-		routeTablePrivate, createNatRouteTableErr := network.CreatePrivateRouteTable(ctx, projectName, indexNum, vpcResource, "private", "0.0.0.0/0", natGateways[cidrRange])
-		fmt.Printf("Created Route Table for CIDR %s\n", cidrRange)
+	//for i, cidrRange := range sortedCidrRanges {
+	indexNumTemp := "0"
+	for cidrRange, natGateway := range natGateways {
+		fmt.Println(pulumi.StringInput(natGateway.ID()))
+		//indexNum := strconv.Itoa(i + 1)
+
+		// TODO: HERE IS THE PROBLEM BECAUSE NAT KEEPS GETTING ASSIGNED TO A DIFFERENT ROUTE TABLE EVERY TIME !!!
+		routeTablePrivate, createNatRouteTableErr := network.CreatePrivateRouteTable(ctx, projectName, indexNumTemp, vpcResource, "private", "0.0.0.0/0", natGateways[cidrRange])
 		if createNatRouteTableErr != nil {
 			return createNatRouteTableErr
 		}
-		listOfPrivateRouteTables = append(listOfPrivateRouteTables, routeTablePrivate)
+		counter, _ := strconv.Atoi(indexNumTemp)
+		indexNumTemp = strconv.Itoa(counter + 1)
+		// debug
+		//fmt.Printf("Created Route Table for CIDR %s\n", cidrRange)
+		//routeTablePrivate.ID().ApplyT(func(id string) error {
+		//fmt.Printf("route table id: %s\n", id)
+		//return nil
+		//})
 
-		_, associateRouteTableErr := network.AssociateRouteTable(ctx, projectName, indexNum, privateSubnets[cidrRange], "private", routeTablePrivate)
-		fmt.Println("Associated Route Table with subnet: ", privateSubnets[cidrRange])
+		listOfPrivateRouteTables = append(listOfPrivateRouteTables, routeTablePrivate)
+		sortedRouteTables[cidrRange] = routeTablePrivate
+	}
+
+	indexNumTemp1 := "0"
+	for cidrRange, routeTable := range sortedRouteTables {
+		//indexNum := strconv.Itoa(i + 1)
+		routeTableAssocResource, associateRouteTableErr := network.AssociateRouteTable(ctx, projectName, indexNumTemp1, privateSubnets[cidrRange], "private", routeTable)
 		if associateRouteTableErr != nil {
 			return associateRouteTableErr
 		}
+		counter, _ := strconv.Atoi(indexNumTemp1)
+		indexNumTemp1 = strconv.Itoa(counter + 1)
+		// debug
+		//fmt.Println("Associated Route Table with subnet: ", privateSubnets[cidrRange])
+		fmt.Println(cidrRange + "\n")
+		routeTable.ID().ApplyT(func(id string) error {
+			fmt.Printf("%s -> %s\n", cidrRange, id)
+			return nil
+		})
+		routeTableAssocResource.ID().ApplyT(func(id string) error {
+			fmt.Printf("route table assoc id: %s\n", id)
+			return nil
+		})
 	}
 
 	_, createS3VpcEndpoint := network.CreateS3VpcEndpoint(ctx, projectName, mainRegion, vpcResource, listOfPrivateRouteTables)
@@ -122,7 +157,5 @@ func Network(ctx *pulumi.Context, projectName string, mainRegion string, vpcCidr
 		return createDynamoDBVpcEndpoint
 	}
 
-	// TODO : check what to do with exports and if we need them at all
-	ctx.Export("vpcResource", vpcResource)
 	return nil
 }
